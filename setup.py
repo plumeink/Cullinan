764164ca30a22c4450575ce69cb3e307684f2353
hansion <hansion.fnep@gmail.com>
2019-04-12 15:09:54 +0800

    update - add package-pymysql

--- setup.py
+++ setup.py
@@ -9,11 +9,11 @@ from setuptools import setup
 
 setup(
     name='cullinan',
-    version='0.1.3',
+    version='0.1.4',
     packages=['cullinan'],
     description='cullinan',
     author='fnep_tech',
     author_email='hansion.fnep@gmail.com',
     url='https://www.fnep-tech.com/',
-    install_requires=['tornado', 'python-dotenv', 'sqlalchemy'],
+    install_requires=['tornado', 'python-dotenv', 'sqlalchemy', 'pymysql'],
 )
