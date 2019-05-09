d6bb69c66a7e7b4df88ead1fb370b9a747fceda9
hansion <hansion.fnep@gmail.com>
2019-05-10 01:31:30 +0800

    update - orm - add mysql type(INTEGER,VARCHAR...) - Ver. 0.1.8

--- cullinan/dao.py
+++ cullinan/dao.py
@@ -7,6 +7,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import *
+from sqlalchemy.dialects.mysql import *
 import os
 
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='cullinan',
-    version='0.1.7',
+    version='0.1.8',
     packages=['cullinan'],
     description='cullinan',
     author='fnep_tech',
