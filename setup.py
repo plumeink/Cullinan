853a59ce0044f95f4d9f09eba9d1a449ffd0d175
Hansion <hansion@fnep-tech.com>
2019-09-18 23:16:27 +0800

    add description - Ver.0.2.10

--- setup.py
+++ setup.py
@@ -7,12 +7,17 @@
 
 from setuptools import setup
 
+with open("README.MD", "r", encoding="UTF-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='cullinan',
-    version='0.2.9',
+    version='0.2.10',
     packages=['cullinan'],
     description='cullinan',
     author='fnep_tech',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author_email='hansion@fnep-tech.com',
     url='https://www.fnep-tech.com/',
     license='http://www.apache.org/licenses/LICENSE-2.0',
