46edd2a083255052836e2b9bead7e44713c7d8df
hansion <hansion@orestu.com>
2020-07-30 14:28:15 +0800

    fix desc & fix version & change build type - Ver.0.31

--- setup.py
+++ setup.py
@@ -12,14 +12,24 @@ with open("README.MD", "r", encoding="UTF-8") as fh:
 
 setup(
     name='cullinan',
-    version='0.30b',
+    version='0.31',
     packages=['cullinan'],
-    description='cullinan',
+    description='a simple web framework',
     author='ore_studio',
+    project_urls={
+            'Source': 'https://github.com/orestu/Cullinan',
+            'Wiki': 'https://github.com/orestu/Cullinan/wiki',
+      },
     long_description=long_description,
     long_description_content_type="text/markdown",
     author_email='python@orestu.com',
-    url='https://cullinan.orestu.com/',
+    url='https://github.com/orestu/Cullinan',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License 2.0",
+        "Operating System :: OS Independent",
+    ],
     license='http://www.apache.org/licenses/LICENSE-2.0',
     install_requires=['tornado', 'python-dotenv', 'sqlalchemy', 'pymysql'],
+    python_requires='>=3'
 )
