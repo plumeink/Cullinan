080d9040727bbf1cb10d7917c7c40c645099eef6
plumeink <hansion@plumeink.com>
2023-04-16 20:38:05 +0800

    optimization README.MD - Ver.0.51

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="UTF-8") as fh:
 
 setup(
     name='cullinan',
-    version='0.50',
+    version='0.51',
     packages=['cullinan'],
     description='Cullinan is written based on tornado and Sqlalchemy to help the project quickly build web application',
     author='plumeink',
