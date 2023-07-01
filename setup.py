3f8c1a6804dd4d8d926f0ac58f3683ac2ab35a97
plumeink <hansion@plumeink.com>
2023-07-02 00:58:18 +0800

    fix bug - Ver.0.54a2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.MD", "r", encoding="UTF-8") as fh:
 
 setup(
     name='cullinan',
-    version='0.54a1',
+    version='0.54a2',
     packages=['cullinan'],
     description='Cullinan is written based on tornado and Sqlalchemy to help the project quickly build web application',
     author='plumeink',
