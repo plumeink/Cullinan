424d2474c26fec72a418ed14216f03abbafcbe7e
plumeink <hansion@plumeink.com>
2025-11-10 22:14:57 +0800

    update to Ver.0.70a1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.MD", "r", encoding="UTF-8") as fh:
 
 setup(
     name='cullinan',
-    version='0.7.0a1',
+    version='0.70a1',
     packages=['cullinan'],
     description='Cullinan is written based on tornado and Sqlalchemy to help the project quickly build web application',
     author='plumeink',
